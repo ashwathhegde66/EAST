1e7e77f760233474e08446e07b332a304390b6f4
ashwathhegde66 <ashwathhegde.66@gmail.com>
2019-07-17 14:05:33 +0530

    txt name

--- icdar.py
+++ icdar.py
@@ -607,7 +607,7 @@ def generator(input_size=512, batch_size=32,
                     continue
 
                 text_polys, text_tags = load_annoataion(txt_fn)
-                print(img_fn)
+                print(txt_fn)
                 text_polys, text_tags = check_and_validate_polys(text_polys, text_tags, (h, w))
                 # if text_polys.shape[0] == 0:
                 #     continue
